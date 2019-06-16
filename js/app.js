4996423adbc16eb787775f77c7ed2971fcd47b3c
ErikMille <miller.erik.1998@gmail>
2019-06-16 13:26:30 +0200

    end

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const deck = new Deck();
+    const place = document.querySelector("#desk");
+    deck.mount(place);
+    
+    button_red=document.querySelector("#red");
+    button_blue=document.querySelector("#blue");
+    button_yellow=document.querySelector("#yellow");
+    button_aqua=document.querySelector("#aqua");
+    button_green=document.querySelector("#green");
+    button_pink=document.querySelector("#pink");
+    button_reset=document.querySelector("#reset");
+
+    button_red.addEventListener("click", () => {
+        deck.change_color_btn(1,0,0);
+    });
+    button_blue.addEventListener("click", () => {
+        deck.change_color_btn(2,0,0);
+    });
+    button_yellow.addEventListener("click", () => {
+        deck.change_color_btn(3,0,0);
+    });
+    button_aqua.addEventListener("click", () => {
+        deck.change_color_btn(4,0,0);
+    });
+    button_green.addEventListener("click", () => {
+        deck.change_color_btn(5,0,0);
+    });
+    button_pink.addEventListener("click", () => {
+        deck.change_color_btn(6,0,0);
+    });
+    button_reset.addEventListener("click", () => {
+        deck.mount(place);
+    });
+
+});
---
+++ js/deck.js
@@ -0,0 +1,70 @@
+
+function randint(min, max) {
+    return Math.round(min + Math.random() * (max - min));
+}
+
+
+class Deck {
+    constructor() {
+        this.cubes=[];
+        this.element;
+    }
+
+    render() {
+        this.element = document.createElement("div");
+        this.element.innerHTML = "";
+        this.element.className = "container";
+        for (let i=0;i<14;i++){
+            this.cubes.push([]);
+            for (let j=0;j<14;j++){
+                this.cubes[i].push(new Cube());
+            }
+        }
+    }
+
+    mount(parent) {
+        parent.innerHTML='';
+        this.render();
+        parent.appendChild(this.element);
+        for (let i=0;i<14;i++){
+            this.cubes.push([]);
+            for (let j=0;j<14;j++){
+                this.cubes[i][j].mount(this.element);
+            }
+        }
+        this.update();
+    }
+
+    update(){
+        let win=true;
+        for (let i=0;i<14;i++){
+            for (let j=0;j<14;j++){
+                this.cubes[i][j].update();
+                if(this.cubes[i][j].color===this.cubes[0][0].color){win=false}
+            }
+        }
+        if(win){alert('WIN!!')}
+    }
+    change_color_btn(col,i,j){
+        for (let i=0;i<14;i++){
+            for (let j=0;j<14;j++){
+                this.cubes[i][j].team=true;
+            }
+        }
+        this.change_color(col,i,j);
+        
+        
+    }
+    change_color(col,i,j){
+        
+        this.cubes[i][j].team=false;
+            if ((i>0)&&(this.cubes[i-1][j].color===this.cubes[i][j].color)){if(this.cubes[i-1][j].team===true){this.change_color(col,i-1,j);}}
+            if ((i<13)&&(this.cubes[i+1][j].color===this.cubes[i][j].color)){if(this.cubes[i+1][j].team===true){this.change_color(col,i+1,j);}}
+            if ((j>0)&&(this.cubes[i][j-1].color===this.cubes[i][j].color)){if(this.cubes[i][j-1].team===true){this.change_color(col,i,j-1);}}
+            if ((j<13)&&(this.cubes[i][j+1].color===this.cubes[i][j].color)){if(this.cubes[i][j+1].team===true){this.change_color(col,i,j+1);}}
+            
+        this.cubes[i][j].color=col;
+        this.update();
+    }
+
+}
